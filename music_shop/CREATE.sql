fe83f9efc5bf42fc9616876bf379f81f8b50f453
VladIakimenko <v_yakimenko@inbox.ru>
2023-02-03 16:15:40 +0400

    INSERT and  SELECT

---
+++ music_shop/CREATE.sql
@@ -0,0 +1,71 @@
+CREATE TABLE genres 
+(
+    PRIMARY KEY (genre_id),
+    genre_id    SERIAL,
+    genre_name  VARCHAR(60)  NOT NULL  UNIQUE
+);
+
+CREATE TABLE artists 
+(
+    PRIMARY KEY (artist_id),
+    artist_id    SERIAL,
+    artist_name  VARCHAR(60)  NOT NULL  UNIQUE
+);
+
+CREATE TABLE artists_genres 
+(
+    PRIMARY KEY (relationship_id),
+    relationship_id  SERIAL,
+    artist_id        INTEGER  NOT NULL  REFERENCES artists(artist_id),
+    genre_id         INTEGER  NOT NULL  REFERENCES genres(genre_id)
+);
+
+CREATE TABLE albums 
+(
+    PRIMARY KEY (album_id),
+    album_id      SERIAL,
+    album_name    VARCHAR(60)  NOT NULL  UNIQUE,
+    release_date  DATE,        
+                  CONSTRAINT release_date_range
+                  CHECK (release_date BETWEEN '1900-01-01' 
+                                          AND  current_date)
+);
+
+CREATE TABLE albums_artists
+(
+    PRIMARY KEY (relationship_id),
+    relationship_id  SERIAL,
+    artist_id        INTEGER  NOT NULL  REFERENCES artists(artist_id),
+    album_id         INTEGER  NOT NULL  REFERENCES albums(album_id)
+);
+
+CREATE TABLE tracks 
+(
+    PRIMARY KEY (track_id),
+    track_id     SERIAL,
+    track_name   VARCHAR(60)  NOT NULL,
+    duration     INTEGER      NOT NULL,
+                 CONSTRAINT duration_range
+                 CHECK (duration BETWEEN 0 AND 10800),
+    album_id     INTEGER      NOT NULL  REFERENCES albums(album_id)
+);
+
+CREATE TABLE mixes
+(
+    PRIMARY KEY (mix_id),
+    mix_id       SERIAL,
+    mix_name     VARCHAR(60)  NOT NULL,
+    release_date DATE,        
+                 CONSTRAINT release_date_range
+                 CHECK (release_date BETWEEN '1900-01-01' 
+                                         AND  current_date)
+);
+
+CREATE TABLE tracks_mixes 
+(
+    PRIMARY KEY (relationship_id),
+    relationship_id  SERIAL,
+    track_id         INTEGER  NOT NULL  REFERENCES tracks(track_id),
+    mix_id           INTEGER  NOT NULL  REFERENCES mixes(mix_id)
+);
+
---
+++ music_shop/SELECT.sql
@@ -0,0 +1,40 @@
+/**    
+название и год выхода альбомов, вышедших в 2018 году;
+название и продолжительность самого длительного трека;
+название треков, продолжительность которых не менее 3,5 минуты;
+названия сборников, вышедших в период с 2018 по 2020 год включительно;
+исполнители, чье имя состоит из 1 слова;
+название треков, которые содержат слово "мой"/"my".
+**/
+
+SELECT album_name, DATE_PART('year', release_date)
+  FROM albums
+ WHERE release_date BETWEEN '2018-01-01'
+ 						AND '2018-12-31';
+ 						
+SELECT track_name, duration
+  FROM tracks
+ ORDER BY duration DESC
+ LIMIT 1;
+ 
+SELECT track_name, 
+	   FLOOR(duration / 60) ||':'|| 
+	   TO_CHAR(MOD(duration, 60), 'FM09') 
+	   AS duration
+  FROM tracks
+ WHERE duration >= 210;
+ 
+SELECT mix_name
+  FROM mixes
+ WHERE DATE_PART('year', release_date) BETWEEN 2018
+  										   AND 2020;
+  										    
+SELECT artist_name
+  FROM artists
+ WHERE SPLIT_PART(artist_name, ' ', 2) = '';
+ 
+SELECT track_name
+  FROM tracks
+ WHERE track_name ILIKE '%my%'
+ 	OR track_name ILIKE '%мой%';
+ 	  
